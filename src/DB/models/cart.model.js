cfa7aff411ffc3362b506278818b6786de06b3c6
mohamed <mohamedelhady874@gmail.com>
2026-03-13 17:44:40 +0200

    add:CartSchema

---
+++ src/DB/models/cart.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const cartItemSchema = new mongoose.Schema(
+  {
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+  
+    quantity: {
+      type: Number,                  //     user
+      default: 1,                    //       |
+      min: 1,                        //       |____cart
+    },                               //              | 
+                                     //              |______items[]
+  },                                 //                        |
+  { _id: false }                     //                        |______product
+);
+const cartSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,      // (1) user ===> (1) cart 
+    },
+
+    items: [cartItemSchema],
+  },
+  { timestamps: true }
+);
+
+const Cart = mongoose.models.Cart || mongoose.model("Cart", cartSchema);
+
+export default Cart;
