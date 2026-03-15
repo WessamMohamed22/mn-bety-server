0ebc6f59121a2686ec19309f1db9dd4f566bed3a
Ahmed-EsmaiL-web <ahmedmohamedismail2001@gmail.com>
2026-03-16 01:28:19 +0200

    Feat: add methods for getUserCart and addToCart

---
+++ src/modules/cart/cart.service.js
@@ -0,0 +1,56 @@
+import Cart from "../../DB/models/cart.model.js";
+import Product from "../../DB/models/product.model.js";
+import { MESSAGES } from "../../constants/messages.js";
+import { createBadRequestError, createNotFoundError } from "../../errors/error.factory.js";
+
+/**
+ * @desc    Get user's cart
+ * @param   {string} userId 
+ */
+export const getUserCart =async (userId) =>{
+    let cart = await Cart.findOne({ user: userId }).populate({
+        path: "items.product",
+        select: "name price discountPrice images slug stock seller",
+    });
+    return cart || { user: userId, items: [] };
+};
+
+/**
+ * @desc    Add product to cart or increase quantity
+ * @param   {string} userId 
+ * @param   {string} productId 
+ * @param   {number} quantity 
+ */
+export const addToCart = async (userId, productId, quantity=1) => {
+    const product = await Product.findById(productId);
+    if (!product) {
+        throw createNotFoundError(MESSAGES.PRODUCT_NOT_FOUND);
+    }
+    if (quantity > product.stock) {
+        throw createBadRequestError(`Only ${product.stock} items left in stock`);
+    }
+    let cart = await Cart.findOne({ user: userId });
+    if (!cart) {
+        cart = await Cart.create({ 
+            user: userId, 
+            items: [{ product: productId, quantity }] 
+        });
+        return cart;
+    }
+    const productIndex = cart.items.findIndex(
+        (item) => item.product.toString() === productId.toString()
+    );
+
+    if (productIndex > -1) {
+        const newQuantity = cart.items[productIndex].quantity + quantity;
+        if (product.stock < newQuantity) {
+            throw createBadRequestError(`Cannot add more. Stock limit: ${product.stock}`);
+        }
+        cart.items[productIndex].quantity = newQuantity;
+    } else {
+        cart.items.push({ product: productId, quantity });
+    }
+
+    await cart.save();
+    return cart;
+}
