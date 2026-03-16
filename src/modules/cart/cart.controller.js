688b84d08842988f3cd9a0410e6023f919ff4062
Ahmed-EsmaiL-web <ahmedmohamedismail2001@gmail.com>
2026-03-16 02:43:46 +0200

    Feat: Adding methods for add to cart , remove from it and clear all of data from it

--- src/modules/cart/cart.controller.js
+++ src/modules/cart/cart.controller.js
@@ -10,3 +10,32 @@ export const getUserCart = asyncHandler(async(req , res)=>{
         .status(HTTP_STATUS.OK)
         .json(successResponse({ cart }, "Cart fetched successfully"));
 })
+
+export const addToCart = asyncHandler(async (req, res) => {
+  const userId = req.decoded.userId;
+  const { productId, quantity } = req.body;
+
+  const cart = await CartService.addToCart(userId, productId, quantity);
+
+  return res
+    .status(HTTP_STATUS.OK)
+    .json(successResponse({ cart }, "Product added to cart"));
+});
+
+export const removeFromCart = asyncHandler(async (req, res) => {
+  const userId = req.decoded.userId;
+  const { productId } = req.params;
+
+  const cart = await CartService.removeFromCart(userId, productId);
+
+  return res
+    .status(HTTP_STATUS.OK)
+    .json(successResponse({ cart }, "Product removed from cart"));
+});
+
+export const clearCart = asyncHandler(async (req, res) => {
+  const userId = req.decoded.userId;
+  await CartService.clearCart(userId);
+
+  return res.status(HTTP_STATUS.NO_CONTENT).end();
+});
