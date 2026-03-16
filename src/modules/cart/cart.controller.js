99a69c46cff4cc6ba12e1fd22d4fbbce7c6e31e1
Ahmed-EsmaiL-web <ahmedmohamedismail2001@gmail.com>
2026-03-16 02:57:24 +0200

    Feat: adding updating function to the controller

--- src/modules/cart/cart.controller.js
+++ src/modules/cart/cart.controller.js
@@ -3,13 +3,13 @@ import asyncHandler from "../../middlewares/asynHandler.js";
 import { HTTP_STATUS } from "../../constants/httpStatus.js";
 import { successResponse } from "../../utils/apiResponse.util.js";
 
-export const getUserCart = asyncHandler(async(req , res)=>{
-    const userId = req.decoded.id;
-    const cart = await CartService.getUserCart(userId);
-    return res
-        .status(HTTP_STATUS.OK)
-        .json(successResponse({ cart }, "Cart fetched successfully"));
-})
+export const getUserCart = asyncHandler(async (req, res) => {
+  const userId = req.decoded.id;
+  const cart = await CartService.getUserCart(userId);
+  return res
+    .status(HTTP_STATUS.OK)
+    .json(successResponse({ cart }, "Cart fetched successfully"));
+});
 
 export const addToCart = asyncHandler(async (req, res) => {
   const userId = req.decoded.userId;
@@ -20,6 +20,22 @@ export const addToCart = asyncHandler(async (req, res) => {
   return res
     .status(HTTP_STATUS.OK)
     .json(successResponse({ cart }, "Product added to cart"));
+});
+
+export const updateCartItemQuantity = asyncHandler(async (req, res) => {
+  const userId = req.decoded.userId;
+  const { productId } = req.params; // ID from the URL
+  const { quantity } = req.body; // New quantity from the body
+
+  const cart = await CartService.updateCartItemQuantity(
+    userId,
+    productId,
+    quantity,
+  );
+
+  return res
+    .status(HTTP_STATUS.OK)
+    .json(successResponse({ cart }, "Cart quantity updated successfully"));
 });
 
 export const removeFromCart = asyncHandler(async (req, res) => {
