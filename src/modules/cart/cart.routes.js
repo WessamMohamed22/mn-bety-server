6f6e4eb1f3a3c130a9b8dea3fa53ada057bc429d
Ahmed-EsmaiL-web <ahmedmohamedismail2001@gmail.com>
2026-03-16 02:48:17 +0200

    Feat: Setup cart routing and end points

---
+++ src/modules/cart/cart.routes.js
@@ -0,0 +1,26 @@
+import express from "express";
+import {
+  getUserCart,
+  addToCart,
+  removeFromCart,
+  clearCart,
+} from "./cart.controller.js";
+import {
+  validateAddToCart,
+  validateProductIdParam,
+} from "./cart.validation.js";
+import { verifyAccessMW } from "../../middlewares/verifyAccessMW.js";
+import { verifyPermissionsMW } from "../../middlewares/verifyPermissionsMW.js";
+import { ROLES } from "../../constants/roles.js";
+
+const router = express.Router();
+
+// User must be logged in and have the 'USER' role to use the cart
+router.use(verifyAccessMW, verifyPermissionsMW([ROLES.USER]));
+
+router.get("/", getUserCart);
+router.post("/", validateAddToCart, addToCart);
+router.delete("/", clearCart);
+router.delete("/:productId", validateProductIdParam, removeFromCart);
+
+export default router;
