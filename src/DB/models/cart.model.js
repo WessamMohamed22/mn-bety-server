7b5fb1464646e1e06d18fc2d3aabd76d6ce8d7fb
Ahmed-EsmaiL-web <ahmedmohamedismail2001@gmail.com>
2026-03-24 16:11:12 +0200

    Fix : Name of User Id in schema

--- src/DB/models/cart.model.js
+++ src/DB/models/cart.model.js
@@ -19,7 +19,7 @@ const cartItemSchema = new mongoose.Schema(
 );
 const cartSchema = new mongoose.Schema(
   {
-    user: {
+    userId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
