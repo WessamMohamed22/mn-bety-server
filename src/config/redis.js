2e892134340e789eb370b9b93c3484e0b64bbda1
Ahmed-EsmaiL-web <ahmedmohamedismail2001@gmail.com>
2026-03-27 18:03:53 +0200

    Update cart service to be cashed and have limit time and deleted if it's out of stock

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,5 +1,5 @@
 import { createClient } from "redis";
-import { env } from "./env";
+import { env } from "./env.js";
 
 // Connects to default localhost:6379, or your cloud Redis URL
 const redisClient = createClient({
